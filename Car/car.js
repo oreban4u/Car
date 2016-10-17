016cc6695e19b9f652be2dcb50351bc178ea65ac
Oreoluwa Banwo <oreban4u@yahoo.co.uk>
2016-10-17 20:43:31 +0100

    Another Commit

--- Car/car.js
+++ Car/car.js
@@ -9,25 +9,18 @@ function Car(name, model, type) {
       this.name = name;
     }
     
-    if(name === 'Porshe' || name === 'Koenigsegg'){
+    if(this.name === 'Porshe' || this.name === 'Koenigsegg'){
      this.numOfDoors = 2;
-     /*this.drive = function(driveno){
-        speed = 50 * driveno;
-        this.speed = ''+speed+ ' km/h';
-      };*/
+     
     }
     else{
      this.numOfDoors = 4;
     }
-    if(type === 'trailer'){
+    if(this.type === 'trailer'){
       this.numOfWheels = 8;
       this.isSaloon = false;
       this.speed = '0 km/h';
-      /*this.drive = function(driveno){
-        speed = 11 * driveno;
-        this.speed = ''+speed+ ' km/h';
-        return this;
-      };*/
+      
     }
     else{
       this.numOfWheels = 4;
@@ -35,11 +28,12 @@ function Car(name, model, type) {
     }
     this.speed = '0 km/h';
     this.drive = function(driveno) {
-      if (type === 'trailer'){
+      if (this.type === 'trailer'){
         speed = 11 * driveno;
         this.speed = ''+speed+ ' km/h';
       }
-      if (name === 'Porshe' || name === 'Koenigsegg'){
+      
+      else{  
         speed = 50 * driveno;
         this.speed = ''+speed+ ' km/h';
       }
